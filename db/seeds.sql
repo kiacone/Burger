a3f79a195be405436a3cd0b125200bce2b77124c
Kristin Iacone <kiacone@gmail.com>
2018-05-07 21:20:49 -0400

    Burger homework

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheesburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
